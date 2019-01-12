9ec8d9316e3cc60966f523545e050be8cd80a7d4
j.hajiani <j.hajiani@karinaco.com>
2019-01-12 17:42:57 +0330

    get coins

--- web/urls.py
+++ web/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from django.views.generic import RedirectView
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 from web.views import *
 
 urlpatterns = [
     path('', redirect_view),
     path('home/', home),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- web/views.py
+++ web/views.py
@@ -1,13 +1,19 @@
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
-
+import requests
 @csrf_exempt
 def redirect_view(request):
     response = redirect("/{}/".format("home"))
     return response
 @csrf_exempt
 def home(request):
-    body={"name":"test", "family":"test2"}
-    response={"body": body, "title": "Exchanger"}
+    coins = getcoins()
+    response={"n": range(1,20), "title": "Exchanger", "coins": coins , "mydict" : {"hi": "Hello"} }
+    print(coins['1'])
     return render(request,"home.html", context=response)
+
+
+def getcoins():
+    coin=requests.get("https://chasing-coins.com/api/v1/top-coins/20").json()
+    return coin
