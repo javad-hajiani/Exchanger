ffe8f35e89fd92c35bfb7d3075954a92e7a6717b
j.hajiani <j.hajiani@karinaco.com>
2019-01-13 09:25:47 +0330

    add rial api

--- web/views.py
+++ web/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
@@ -8,12 +10,14 @@ def redirect_view(request):
     return response
 @csrf_exempt
 def home(request):
-    coins = getcoins()
-    response={"n": range(1,20), "title": "Exchanger", "coins": coins , "mydict" : {"hi": "Hello"} }
-    print(coins['1'])
+    response={"n": range(1,20), "title": "Exchanger", "coins": getcoins() , "dollar" : getdollar() }
     return render(request,"home.html", context=response)
 
 
 def getcoins():
     coin=requests.get("https://chasing-coins.com/api/v1/top-coins/20").json()
     return coin
+
+def getdollar():
+    dollar=requests.post('https://payment24.ir/api/product/currency-price-calculation?product=cards_buy','{"product": "cards_buy"}').json()
+    return dollar["data"]["currencies"]["USD"]
